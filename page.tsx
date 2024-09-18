4e1539acf1dfcae0f065d50b2273c57cabdd6b1c
Hira shaikh <160590123+haimishaikh149@users.noreply.github.com>
2024-09-18 23:30:14 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,14 @@
+
+import Header from '@/app/components/header'
+import About from './components/about';
+
+export default function Home() {
+  return (
+    <>
+    <h1>This is Home Page</h1>
+    < Header />
+    < About/>
+    </>
+    
+  );
+}
